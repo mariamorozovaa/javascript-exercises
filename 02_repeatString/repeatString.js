b10dc3fb91e3326d5fa56eb39d8ada2b87f4d926
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-01-01 14:46:33 +0300

    Do the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newStr = '';
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            newStr += string;
+        }    
+    }
+    else {
+        newStr = 'ERROR';
+    }
+    return newStr;
 };
 
 // Do not edit below this line
