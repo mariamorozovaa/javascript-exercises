f17198da9d903af90b40ccb1683be5f6d4501143
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-01-09 20:36:04 +0300

    Do the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let result = 0;
+
+  result = 5 / 9 * (tempFahrenheit - 32);
+  result = Number(result.toFixed(1));
+
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let result = 0;
+
+  result = 9 / 5 * tempCelsius + 32;
+  result = Number(result.toFixed(1));
+
+  return result;
 };
 
 // Do not edit below this line
