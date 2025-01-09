6ddd5c9186503c09640fc3049ced6891ff68c93c
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-01-09 12:22:33 +0300

    Do the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sumOfAll = 0;
 
+    if ((start < end) && (start >= 0) && (end >= 0) && (Number.isInteger(start)) && (Number.isInteger(end))) {
+        for (i = start; i <= end; i++) {
+            sumOfAll += i;
+        }
+    }
+
+    else if ((start > end) && (start >= 0) && (end >= 0) && (Number.isInteger(start)) && (Number.isInteger(end))) {
+        for (i = end; i <= start; i++) {
+            sumOfAll += i;
+        }
+    }
+
+    else {
+        sumOfAll = 'ERROR';
+    }
+    
+    return sumOfAll;
 };
 
 // Do not edit below this line
