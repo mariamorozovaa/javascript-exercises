78720a383bdafd0720aa6b9d59277edead4b321c
Maria Morozova <59123858+mariamorozovaa@users.noreply.github.com>
2025-01-09 20:09:37 +0300

    Do the exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let result = false;
 
+    if (((year % 4 === 0) && !(year % 100 === 0)) || (year % 400 === 0)) {
+        result = true;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
